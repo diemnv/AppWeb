61b5c7a6335a3581fc7b2ae668e58f27e8e4df13
User <User@DESKTOP-PGKHSCB>
2019-01-16 11:06:54 +0700

    final

--- src/main/java/com/cmc/gdp05/group4/repository/NewsRepository.java
+++ src/main/java/com/cmc/gdp05/group4/repository/NewsRepository.java
@@ -10,5 +10,15 @@ import com.cmc.gdp05.group4.entity.News;
 @Repository("newsRepository")
 public interface NewsRepository extends JpaRepository<News, Integer> {
 
+	/**
+	 * Create by: dvthuan - CMC
+	 * Create date: Jan 16, 2019
+	 * Modifier: dvthuan
+	 * Modified date: Jan 16, 2019
+	 * Description: ....
+	 * Version 1.0
+	 * @param q
+	 * @return
+	 */
 	List<News> findByTitleContaining(String q);
 }

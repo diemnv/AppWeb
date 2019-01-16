12a987398bd9dba9ac26db607e5b1bcf0b7eed78
User <User@DESKTOP-PGKHSCB>
2019-01-16 10:52:59 +0700

    push code

--- src/main/java/com/cmc/gdp05/group4/controller/NewsController.java
+++ src/main/java/com/cmc/gdp05/group4/controller/NewsController.java
@@ -1,17 +1,12 @@
 package com.cmc.gdp05.group4.controller;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
 
 import com.cmc.gdp05.group4.entity.News;
 import com.cmc.gdp05.group4.service.NewsService;
@@ -22,53 +17,42 @@ public class NewsController {
 	@Autowired
 	NewsService newsService;
 
-	/**
-	 * @return
-	 */
-	@GetMapping({ "/news" })
-	public ModelAndView news() {
-		ModelAndView mvn = new ModelAndView();
-		List<News> listNews = new ArrayList<>();
-		listNews = newsService.getAllNews();
-		mvn.addObject("listNews", listNews);
-		mvn.setViewName("news");
-		return mvn;
+	@GetMapping({ "/manager/news/list" })
+	public String listNews(Model model) {
+		model.addAttribute("listNews", newsService.getAllNews());
+		return "admin/news/list";
 	}
 
-	@GetMapping("/addnews")
+	@GetMapping("/manager/news/add")
 	public String addNews(News news) {
-		return "addnews";
+		return "admin/news/add";
 	}
 
 	@PostMapping("/save")
 	public String saveNews(News news) {
-		System.err.println("==============================================");
 //		news.setIdUser(1);
 //		news.setIdApprover(1);
 		newsService.saveOneNews(news);
-		return "redirect:/news";
+		return "redirect:/manager/news/list";
 	}
 
 	@GetMapping("/news/delete/{id}")
 	public String deleteOneNews(@PathVariable int id) {
 		newsService.deleteOneNews(id);
-		return "redirect:/news";
+		return "redirect:/manager/news/list";
 	}
 
 	@GetMapping("/news/edit/{id}")
 	public String editOneNews(@PathVariable int id, Model model) {
 		model.addAttribute("news", newsService.getOneNews(id));
-		return "addnews";
+		return "admin/news/add";
 	}
 
 	@PostMapping(value = "/search")
 	public String search(@RequestParam("keyword") String keyword, Model model) {
-		if ("".equals(keyword)) {
-			return "redirect:/news";
-		}
 		System.err.println(newsService.searchNews(keyword).size());
 		model.addAttribute("listNews", newsService.searchNews(keyword));
-		return "news";
+		return "admin/news/list";
 	}
 
 }
